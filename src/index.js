f4a06c22eeb7b860d16ef51b2d7c44c779e2a7b9
Nik79 <niknikcor@gmail.com>
2018-03-05 09:04:05 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+var zerosNum = 0;
+var degree = 1;
+var div = number/Math.pow(5,1);
+
+while (div>1) {
+	var flooredDiv = Math.floor(div);
+	zerosNum = zerosNum + div;
+	degree = degree + 1;
+	div = number/Math.pow(5,degree);
+}
+
+return zerosNum;
+
 }
